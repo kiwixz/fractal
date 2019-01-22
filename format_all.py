7bf0f68f697974c2ec0512c58094b0c5b1c0cb89
kiwixz <kiwixz@outlook.com>
2019-01-22 20:17:05 +0000

    scripts subprocess proper calls

--- format_all.py
+++ format_all.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
                         level=logging.DEBUG)
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    git_files = subprocess.check_output(r'git ls-files').decode().splitlines()
+    git_files = subprocess.check_output(["git", "ls-files"]).decode().splitlines()
     for f in git_files:
         if f.endswith(".cpp") or f.endswith(".h") or f.endswith(".inl"):
             logging.info(f"formatting: {f}")
-            subprocess.check_call(rf'clang-format -i {f}')
+            subprocess.check_call(["clang-format", "-i", f])
--- install_libs.py
+++ install_libs.py
@@ -9,10 +9,10 @@ from pathlib import Path
 
 
 VCPKG_COMMIT = "c6db2c880a064b29e0b38eda29f19510235fb4c9"
-VCPKG_LIBS = " ".join([
+VCPKG_LIBS = [
     "fmt",
     "spdlog",
-])
+]
 
 
 def is_win():
@@ -23,23 +23,23 @@ def install_vcpkg():
     logging.info("installing vcpkg")
     if os.path.exists(Path("vcpkg") / ".git"):
         logging.warning("vcpkg seems already installed, trying to update")
-        if subprocess.check_output(r'git -C "vcpkg" rev-parse HEAD').decode()[:-1] == VCPKG_COMMIT:
+        if subprocess.check_output(["git", "-C", "vcpkg", "rev-parse", "HEAD"]).decode()[:-1] == VCPKG_COMMIT:
             logging.warning("vcpkg seems already up-to-date, skipping")
             return
-        subprocess.check_call(r'git -C "vcpkg" fetch -p')
+        subprocess.check_call(["git", "-C", "vcpkg", "fetch", "-p"])
     else:
-        subprocess.check_call(r'git clone "https://github.com/Microsoft/vcpkg.git"')
-    subprocess.check_call(rf'git -C "vcpkg" checkout "{VCPKG_COMMIT}"')
+        subprocess.check_call(["git", "clone", "https://github.com/Microsoft/vcpkg.git"])
+    subprocess.check_call(["git", "-C", "vcpkg", "checkout", VCPKG_COMMIT])
     bootstrap_file = Path("vcpkg") / ("bootstrap-vcpkg.bat" if is_win() else "bootstrap-vcpkg.sh")
-    subprocess.check_call(rf'{bootstrap_file} -disableMetrics')
+    subprocess.check_call([bootstrap_file, "-disableMetrics"])
 
 
 def install_vcpkg_packages():
     logging.info("installing vcpkg libs")
     triplet = "x64-windows" if is_win() else "x64-linux"
-    vcpkg_exe = Path("vcpkg") / "vcpkg"
-    subprocess.check_call(rf'{vcpkg_exe} install --triplet {triplet} {VCPKG_LIBS}')
-    subprocess.check_call(rf'{vcpkg_exe} upgrade --no-dry-run')
+    vcpkg_exe = str(Path("vcpkg") / "vcpkg")
+    subprocess.check_call([vcpkg_exe, "install", "--triplet", triplet] + VCPKG_LIBS)
+    subprocess.check_call([vcpkg_exe, "upgrade", "--no-dry-run"])
 
 
 def cleanup_vcpkg():
