c5bae031fa51cd3282eb6df6223a0a7e8e65797c
kiwixz <kiwixz@outlook.com>
2019-01-25 18:32:31 +0000

    gl_object static deleter, gl_array_object

--- fractal/gl_object.h
+++ fractal/gl_object.h
@@ -1,23 +1,43 @@
 #pragma once
 
 #include <glad/glad.h>
+#include <array>
+#include <functional>
 #include <memory>
 
-namespace fractal
-{
+namespace fractal {
 
+using GlDeleter = void (*)(GLuint id);
+using GlArrayDeleter = void (*)(GLsizei size, GLuint id);
+
+
+template <GlDeleter Tdeleter>
 struct GlObject {
-    GlObject();
+    static constexpr GlDeleter deleter = Tdeleter;
 
-    template <typename T>
-    GlObject(GLuint id, T&& deleter);
+    GlObject();
 
     operator GLuint() const;
     GLuint id() const;
 
 private:
-    std::unique_ptr<void, void (*)(void*)> handle_;
+    std::unique_ptr<void, std::function<void(void*)>> handle_;
     GLuint id_;
+};
+
+
+template <GlArrayDeleter Tdeleter, size_t Tsize = 1>
+struct GlArrayObject {
+    static constexpr GlDeleter deleter = Tdeleter;
+    static constexpr size_t size = Tsize;
+
+    GlArrayObject();
+
+    GLuint operator[](size_t index) const;
+
+private:
+    std::unique_ptr<void, std::function<void(void*)>> handle_;
+    std::array<GLuint, size> ids_;
 };
 
 }  // namespace fractal
--- fractal/gl_object.inl
+++ fractal/gl_object.inl
@@ -1,12 +1,38 @@
 #include "gl_object.h"
 
-namespace fractal
-{
+namespace fractal {
 
-template <typename T>
-GlObject::GlObject(GLuint id, T&& deleter) :
-    handle_{nullptr, std::forward<T>(deleter)},
-    id_{id}
+template <GlDeleter Tdeleter>
+GlObject<Tdeleter>::GlObject() :
+    handle_{nullptr, [this](auto) {
+                deleter(id_);
+            }}
 {}
+
+template <GlDeleter Tdeleter>
+GlObject<Tdeleter>::operator GLuint() const
+{
+    return id_;
+}
+
+template <GlDeleter Tdeleter>
+GLuint GlObject<Tdeleter>::id() const
+{
+    return id_;
+}
+
+
+template <GlArrayDeleter Tdeleter, size_t Tsize>
+GlArrayObject<Tdeleter, Tsize>::GlArrayObject() :
+    handle_{nullptr, [this](auto) {
+                deleter(size, ids_.data());
+            }}
+{}
+
+template <GlArrayDeleter Tdeleter, size_t Tsize>
+GLuint GlArrayObject<Tdeleter, Tsize>::operator[](size_t index) const
+{
+    return ids_[index];
+}
 
 }  // namespace fractal
