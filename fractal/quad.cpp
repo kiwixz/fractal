5072bfe6cc8195449a671535bca0929b601a2186
kiwixz <kiwixz@outlook.com>
2019-01-25 19:23:01 +0000

    add incomplete quad class

---
+++ fractal/quad.cpp
@@ -0,0 +1,29 @@
+#include "quad.h"
+
+namespace fractal {
+
+Quad::Quad()
+{
+    constexpr std::array<GLfloat, 5 * 2> texture_coords{{0.f, 0.f,
+                                                         1.f, 0.f,
+                                                         0.f, 1.f,
+                                                         1.f, 1.f}};
+
+    glGenBuffers(vertex_buffers_.size(), vertex_buffers_.ptr());
+
+    glGenVertexArrays(1, vertex_array_.ptr());
+    glBindVertexArray(vertex_array_);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers_[0]);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers_[1]);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glBufferData(GL_ARRAY_BUFFER, texture_coords.size() * sizeof(GLfloat), texture_coords.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+}  // namespace fractal
---
+++ fractal/quad.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "gl_object.h"
+#include <array>
+
+namespace fractal
+{
+
+class Quad {
+public:
+    Quad();
+
+private:
+    GlVertexArrays<> vertex_array_;
+    GlBuffers<2> vertex_buffers_;
+};
+
+}  // namespace fractal
