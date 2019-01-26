aa6dd74e025f2e27f11dddfc8554bb84fbb5a5ba
kiwixz <kiwixz@outlook.com>
2019-01-26 04:56:11 +0000

    remove inl files

--- fractal/bind_front.h
+++ fractal/bind_front.h
@@ -5,6 +5,19 @@ namespace fractal {
 template <typename T, typename... Args>
 auto bind_front(T&& function, Args&&... args);
 
-}  // namespace fractal
 
-#include "bind_front.inl"
+template <typename Function, typename... Args>
+auto bind_front(Function&& function, Args&&... args)
+{
+    return [function = std::forward<Function>(function),
+            bound_args = std::make_tuple(std::forward<Args>(args)...)](auto&& call_args...) {
+        std::apply([&](auto&&... bound_args) {
+            return std::invoke(function,
+                               std::forward<Args>(bound_args)...,
+                               std::forward<decltype(call_args)>(call_args)...);
+        },
+                   decltype(bound_args){bound_args});
+    };
+}
+
+}  // namespace fractal
