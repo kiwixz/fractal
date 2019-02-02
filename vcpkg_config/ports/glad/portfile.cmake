e6595c519d6752ecac835a8e02d5c6517d7360f3
kiwixz <kiwixz@outlook.com>
2019-02-02 15:57:51 +0000

    glad do not remove khr include files

--- vcpkg_config/ports/glad/portfile.cmake
+++ vcpkg_config/ports/glad/portfile.cmake
@@ -25,5 +25,5 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/glad)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/include/KHR)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/glad/copyright COPYONLY)
