9486b5aaacf13b2ef8942dff253888cc74b05976
kiwixz <kiwixz@outlook.com>
2019-02-16 18:22:25 +0100

    multithreaded clang format

--- check_clang_format.py
+++ check_clang_format.py
@@ -5,6 +5,30 @@ import logging
 import os
 import subprocess
 import sys
+from concurrent.futures import ThreadPoolExecutor
+
+
+def check(path, check_id):
+    with open(path) as f:
+        original = f.read()
+    formatted = subprocess.check_output(["clang-format", "-assume-filename", path],
+                                        input=original, text=True)
+    diff = difflib.unified_diff(original.splitlines(keepends=True),
+                                formatted.splitlines(keepends=True),
+                                path,
+                                "formatted")
+    diff_result = list(diff)
+
+    def result(nr_checks):
+        logging.info(f"[{check_id}/{nr_checks}] checked clang-format: {path}")
+        if not diff_result:
+            return True
+        logging.warning("differences found:")
+        sys.stdout.writelines(diff_result)
+        print()
+        return False
+
+    return result
 
 
 if __name__ == "__main__":
@@ -14,23 +38,19 @@ if __name__ == "__main__":
                         stream=sys.stderr)
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    exit_code = 0
     git_files = subprocess.check_output(["git", "ls-files"], text=True).splitlines()
-    for path in git_files:
-        if path.endswith(".cpp") or path.endswith(".h"):
-            logging.info(f"checking clang-format: {path}")
-            with open(path) as f:
-                original = f.read()
-            formatted = subprocess.check_output(["clang-format", "-assume-filename", path],
-                                                input=original, text=True)
-            diff = difflib.unified_diff(original.splitlines(keepends=True),
-                                        formatted.splitlines(keepends=True),
-                                        path,
-                                        "formatted")
-            diff_result = list(diff)
-            if diff_result:
+    with ThreadPoolExecutor(os.cpu_count()) as executor:
+        results = []
+        count = 0
+        for path in git_files:
+            if path.endswith(".cpp") or path.endswith(".h"):
+                results.append(executor.submit(check, path, count + 1))
+                count += 1
+
+        nr_checks = count
+        count = 0
+        exit_code = 0
+        for future in results:
+            if not future.result()(nr_checks):
                 exit_code = 1
-                logging.warning("differences found:")
-                sys.stdout.writelines(diff_result)
-                print()
-    exit(exit_code)
+        exit(exit_code)
