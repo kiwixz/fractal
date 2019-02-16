5bd3164c132cfa88f3da830c6f6f9f0ecb52f5f4
kiwixz <kiwixz@outlook.com>
2019-02-16 22:28:07 +0000

    add thread_pool tests

---
+++ fractal/thread_pool.test.cpp
@@ -0,0 +1,42 @@
+#include "thread_pool.h"
+#include <doctest/doctest.h>
+#include <memory>
+#include <thread>
+
+namespace fractal::test {
+
+TEST_CASE("thread_pool_submit")
+{
+    ThreadPool pool{std::thread::hardware_concurrency()};
+
+    {
+        bool pass = false;
+        pool.submit([&] {
+                pass = true;
+            })
+                .get();
+        CHECK(pass);
+    }
+
+    {
+        bool pass = false;
+        pool.submit([](bool* pass) {
+                *pass = true;
+            },
+                    &pass)
+                .get();
+        CHECK(pass);
+    }
+}
+
+TEST_CASE("thread_pool_special_submit")
+{
+    ThreadPool pool{std::thread::hardware_concurrency()};
+
+    pool.submit([ptr = std::make_unique<int>()] {});
+    pool.submit([ptr = std::make_shared<int>()]() mutable {
+        ptr.reset();
+    });
+}
+
+}  // namespace fractal::test
