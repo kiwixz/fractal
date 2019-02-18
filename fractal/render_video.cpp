37e4e03551f1dd0fe3fc2d966154896710b28c3f
kiwixz <kiwixz@outlook.com>
2019-02-18 03:38:14 +0000

    real render video

--- fractal/render_video.cpp
+++ fractal/render_video.cpp
@@ -8,23 +8,18 @@ namespace fractal {
 void render_video(Settings const& settings)
 {
     std::ofstream ofs{settings.output_file, std::ios::binary};
-
     Encoder encoder{settings, [&](std::byte const* data, int size) {
                         ofs.write(reinterpret_cast<char const*>(data), size);
                     }};
 
+    Mandelbrot mandelbrot{settings.width, settings.height};
+    double zoom = settings.zoom;
+
     int nr_frames = settings.duration * settings.fps;
-
-    // tmp
-    std::vector<uint32_t> pixels_v(settings.width * settings.height);
-    for (int y = 0; y < settings.height; ++y)
-        for (int x = 0; x < settings.width; ++x)
-            pixels_v[y * settings.width + x] = 0xffff0000
-                                               + ((255 * x / settings.width) << 8)
-                                               + (255 * y / settings.height);
-
-    for (int frame = 0; frame < nr_frames; ++frame)
-        encoder.encode_rgb(pixels_v.data());
+    for (int frame = 0; frame < nr_frames; ++frame) {
+        encoder.encode_rgb(mandelbrot.generate(settings.x, settings.y, zoom));
+        zoom += zoom * settings.zoom_speed / settings.fps;
+    }
     encoder.finish();
 }
 
