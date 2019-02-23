1081f03e88401a91bd81d4a387c98e4ee0854cee
kiwixz <kiwixz@outlook.com>
2019-02-23 18:05:35 +0000

    log progress when rendering video

--- fractal/render_video.cpp
+++ fractal/render_video.cpp
@@ -1,6 +1,7 @@
 #include "render_video.h"
 #include "encoder.h"
 #include "mandelbrot.h"
+#include <spdlog/spdlog.h>
 #include <fstream>
 
 namespace fractal {
@@ -18,6 +19,7 @@ void render_video(Settings const& settings)
     int nr_frames = settings.duration * settings.fps;
     for (int frame = 0; frame < nr_frames; ++frame) {
         encoder.encode_rgb(mandelbrot.generate(settings.x, settings.y, zoom));
+        spdlog::info("[render_video] frame {}/{}", frame, nr_frames);
         zoom += zoom * settings.zoom_speed / settings.fps;
     }
     encoder.finish();
